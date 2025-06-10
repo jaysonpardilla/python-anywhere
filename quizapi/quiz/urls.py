820147b4b436165115a292410dabc31ce4ee5f45
jayson-pardilla <pardillajayson004@gmail.com>
2025-06-10 10:19:16 +0800

    Update urls.py
    filter categories

--- quizapi/quiz/urls.py
+++ quizapi/quiz/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('videos/', AllVideosAPIView.as_view(), name='all_videos'),
     path('videos/<int:id>/', SingleVideoAPIView.as_view(), name='single_video'),
     path('categories/', AllCategoriesAPIView.as_view(), name='all_categories'),
+    path('videos/by-category/', VideosByCategoryAPIView.as_view(), name='videos_by_category'),
 ]
